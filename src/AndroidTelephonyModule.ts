50868827039f394a0334cb9e9ef9dbe612247454
Vic Dang <dnnvu@tma.com.vn>
2025-06-06 17:59:41 +0700

    Update AndroidTelephonyModule.ts

--- src/AndroidTelephonyModule.ts
+++ src/AndroidTelephonyModule.ts
@@ -1,8 +1,24 @@
 import { NativeModule, requireNativeModule } from "expo";
+import { Platform } from "react-native";
+
 declare class AndroidTelephonyModule extends NativeModule {
   getAllCellInfo(): string;
   execute(action: string): string;
 }
 
+let androidTelephonyModule;
+
+if (Platform.OS === "android") {
+    androidTelephonyModule = requireNativeModule<AndroidTelephonyModule>("AndroidTelephony");
+} else {
+    // If not on Android, we can provide a mock or empty implementation.
+    androidTelephonyModule = {
+        getAllCellInfo: () => JSON.stringify([]),
+        execute: (action: string) => {
+            return JSON.stringify({});
+        }
+    };
+}
+
 // This call loads the native module object from the JSI.
-export default requireNativeModule<AndroidTelephonyModule>("AndroidTelephony");
+export default androidTelephonyModule;
